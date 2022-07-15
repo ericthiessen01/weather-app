0a98be4ec54ce7f14961b02e9af778a6ffc94b23
ericthiessen01 <ericthiessen01@gmail.com>
2022-07-14 23:14:46 -0600

    updated to dif weather API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import Search from './components/Search'
+// import Search from './components/Search'
 import Weather from './components/Weather'
 
 
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,22 +3,29 @@ import useGeoLocation from '../hooks/useGeoLocation'
 
 export default function Weather() {
     const [currentWeatherData, setCurrentWeatherData] = useState({})
-    // const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true)
     const weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY
     const loc = useGeoLocation()
 
-    function getCurrentWeatherData() {
-        fetch(`http://api.weatherbit.io/v2.0/current?key=${weatherApiKey}&lat=${loc.coords.lat}&lon=${loc.coords.lon}`)
-            .then(res => res.json())
-            .then(data => {
-                setCurrentWeatherData(data.data)
-                console.log(currentWeatherData[0])
-            })    
+    async function getCurrentWeatherData() {
+        await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${loc.coords.lat}&lon=${loc.coords.lon}&appid=${weatherApiKey}&units=metric`)
+        .then(res => res.json())
+        .then(res => {
+            setCurrentWeatherData(res)
+            console.log(res)
+        })
     }
+
+    const currentTime = Intl.DateTimeFormat('en-US', {
+        hour: 'numeric', 
+        minute: 'numeric', 
+        second: 'numeric'
+    }).format(currentWeatherData.dt)
 
     useEffect(() => {
         if(loc.loaded === true){
             getCurrentWeatherData()
+            setLoading(false)
         }
     }, [loc.loaded])
 
@@ -30,13 +37,11 @@ export default function Weather() {
                 "data not available"}
             </p>
             
-            <ul>
-                {/* <li>{currentWeatherData[0].temp}</li>
-                <li>{currentWeatherData[0].city_name}</li>
-                <li>{currentWeatherData[0].ob_time}</li>
-                <li>{currentWeatherData[0].wind_spd}</li>
-                <li>{currentWeatherData[0].wind_cdir}</li> */}
-            </ul>
+            {!loading &&
+                <p>
+                    {currentTime}
+                </p>
+                }
         </div>
     )
 }
